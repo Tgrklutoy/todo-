f17d7b4a51fce879ef1cf53de5d92a832ebe7fef
Tagir <stagir536@gmail.com>
2025-12-04 21:05:53 +0300

    new css and vss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ const todothing = document.getElementById ("todocolumn");
 const inprogressthing = document.getElementById ("inprogresscolumn");
 const reviewthing = document.getElementById ("rewievcolumn");
 const donething = document.getElementById ("donecolumn");
+const delite = document.getElementById("delitecolumn")
 
 const newbutton = document.getElementById ("new-task");
 
@@ -13,21 +14,25 @@ var todo = document.createElement("option");
 var inprogress = document.createElement("option");
 var review = document.createElement("option");
 var done = document.createElement("option");
+var delite = document.createElement("option")
 var todo = select.appendChild(todo);
 var inprogress = select.appendChild(inprogress);
 var review = select.appendChild(review);
 var done = select.appendChild(done);
-var select = taskElement.appendChild(select)
-var input = taskElement.appendChild(input)
-var taskElement = todothing.appendChild(taskElement)
+var delite = select.appendChild(delite);
+var select = taskElement.appendChild(select);
+var input = taskElement.appendChild(input);
+var taskElement = todothing.appendChild(taskElement);
  todo.value = "todocolumn"
 todo.textContent = "Todo"
  inprogress.value = "inprogresscolumn"
-inprogress.textContent = "In progreaa"
+inprogress.textContent = "In progress"
  review.value = "reviewcolumn"
 review.textContent = "Review"
  done.value = "donecolumn"
 done.textContent = "Done"
+ delite.value = "delitecolumn"
+delite.textContent = "Delete"
 function changing (event){
     console.log(event)
     if (event.target.value == inprogress.value) {
@@ -46,4 +51,11 @@ function changing (event){
 select?.addEventListener ("change",changing )
 };
 newbutton?.addEventListener ("click",newclick);
+function delitething (event){
+        console.log(delitething)
+         if (event.target.value == delite.value){
+           taskElement = delite?.appendChild (taskElement)  
+        }
+        
 
+}
